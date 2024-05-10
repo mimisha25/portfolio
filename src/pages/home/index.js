7cbb14e9773366cc195cf2ed9f0f67f8da88da1a
mimisha25 <49067055+mimisha25@users.noreply.github.com>
2024-05-10 14:12:52 -0400

    Fix bug: modify to latest version of packages

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./style.css";
 import { Helmet, HelmetProvider } from "react-helmet-async";
-import Typical from "react-typical";
+import Typewriter from "react-typewriter-effect";
 import { introdata, meta } from "../../hook";
 import { Link } from "react-router-dom";
 
@@ -15,38 +15,37 @@ export default function Home() {
           <title> {meta.title}</title>
           <meta name="description" content={meta.description} />
         </Helmet>
-        <div className="intro_sec d-block d-lg-flex align-items-center ">
+        <div className="intro_sec d-block d-lg-flex align-items-center">
           <div
-            className="h_bg-image order-1 order-lg-2 "
-            style={{ backgroundImage: `url(https://i.postimg.cc/Fz5TrrbJ/aziza-turkey-2021.jpg)` }}
+            className="h_bg-image order-1 order-lg-2 border rounded-5"
+            style={{ backgroundImage: `url(https://plus.unsplash.com/premium_vector-1734332717463-b2d77c881a31?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTkxfHxkZXZlbG9wZXJ8ZW58MHx8MHx8fDA%3D)` }}
           ></div>
           <div className="text order-2 order-lg-1 h-100 d-lg-flex justify-content-center">
             <div className="align-self-center ">
               <div className="intro mx-auto">
                 <h2 className="mb-1x">{introdata.title}</h2>
                 <h1 className="fluidz-48 mb-1x">
-                  <Typical
-                    steps={[
-                      introdata.animated.first,
-                      1000,
-                      introdata.animated.second,
-                      1000,
-                      introdata.animated.third,
-                      1000,
-                    ]}
-                    loop={Infinity}
-                    wrapper="p"
+                  <Typewriter
+                    textStyle={{
+                      fontWeight: 700,
+                      fontSize: '1em',
+                    }}
+                    startDelay={500}
+                    cursorColor="black"
+                    multiText={[introdata.animated.first, introdata.animated.second, introdata.animated.third]}
+                    multiTextDelay={1000}
+                    typeSpeed={100}
                   />
                 </h1>
                 <p className="mb-1x">{introdata.description}</p>
                 <div className="intro_btn-action pb-5">
                   <Link to="/portfolio" className="text_2">
-                    <div id="button_p" className="ac_btn btn ">
+                    <div id="button_p" className="ac_btn btn rounded-4">
                       My Portfolio
                     </div>
                   </Link>
                   <Link to="/contact">
-                    <div id="button_h" className="ac_btn btn">
+                    <div id="button_h" className="ac_btn btn rounded-4">
                       Contact Me
                     </div>
                   </Link>
